f814962a83c911409a3a536bb088b19393a595be
decryptor007 <smazen98@gmail.com>
2020-09-14 15:48:14 +0200

    update the websie serving from node

--- server/app.js
+++ server/app.js
@@ -4,33 +4,47 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// Routers 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var aboutRouter = require('./routes/about');
+var servicesRouter = require('./routes/services');
+var contactRouter = require('./routes/contact');
 
+// Express 
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+
+// EJS view Engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+
+
+
+// Join to the static folders and files inside public
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use('/', indexRouter);
-// app.use('/users', usersRouter);
-app.use(express.static('../Kernel-z'))
+// Routers uses
+app.use('/', indexRouter);
+app.use('/about', aboutRouter);
+app.use('/services', servicesRouter);
+app.use('/contact-us', contactRouter);
+
+// when you want to serve the website as a static site 
+//app.use(express.static('../Kernel-z'))
 
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -39,7 +53,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-app.listen(port=3000, () => {
+app.listen(port = 3000, () => {
   console.log(`Serve is up on http://localhost:${port}`)
 })
 
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', (req, res, next) => {
+  res.render('index', { title: "hello mario" });
+})
 
+// Submit lead
+router.post('/', function (req, res) {
+  res.send("server recieved the request peacefully");
+  console.log(req.body)
+  // then should store this object in the database 
+});
 module.exports = router;
